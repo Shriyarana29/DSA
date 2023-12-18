994b0c9c9a270dcfb4287d8ff3fa25b00837ee97
SHRIYA RANA <128988513+Shriyarana29@users.noreply.github.com>
2023-12-18 09:21:32 -0800

    Add files via upload

---
+++ binarySearch_usingRecurrsion.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class binarySearch {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of terms in array");;
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements in ascending order");
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter item to be searched");
+        int item = sc.nextInt();
+        int result = binarySearch(arr,item, 0, n-1 );
+        if(result==-1){
+            System.out.println("The element is not present");
+        }
+        else{
+            System.out.println("element found at index "+result);
+        }
+    }
+    public static int binarySearch(int[] arr, int item, int left, int right){
+        while(left<= right){
+            int mid= left + (right - left)/2;
+            if(arr[mid]==item){
+               return mid;
+            }
+            else if(arr[mid]<item){
+                left = mid +1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return -1;
+    }
+}
